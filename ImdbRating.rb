8a42b8457d203337b7e764ec238ad2befb3964b3
jordido <jordi@ubuntu.ubuntu-domain>
2014-09-19 16:51:37 +0200

    initial git (working with movie class)

--- ImdbRating.rb
+++ ImdbRating.rb
@@ -1,41 +1,47 @@
 require 'imdb'
+require_relative 'Movie'
 
 class Rating
 	def initialize (file)
 		@file = file
 		@stars_map = []   # Array of strings (1 per film) containing n-stars 
+		@list_of_films = []
 		load_film_list
 	end
 
 	def load_film_list # for initializing ratings
 		File.open(@file) do |films|
 				films.each do |film_title| 
-					search_rating(film_title)
+					movie = Movie.new(film_title)
+					@stars_map << movie.push_film_stars
+					@list_of_films <<  film_title # OR film_title directly
 				end
+				
 		end
-	end
-
-	def push_film_stars (rating)
-		film_stars = ""
-		film_stars << "*" * rating + " " * (10 - rating)
-		@stars_map << film_stars
 	end
 
 	def print_stars		# last method to be called
 		@print_line = ""
+		puts @stars_map.inspect
 		for i in 9.downto 0
 			@stars_map.each do |x|
-				@print_line << x[i]		# build a line to be printed, containing the i element of each stars_map line
+				@print_line << x[i].to_s		# build a line to be printed, containing the i element of each stars_map line
 			end
 			puts @print_line			# this is the output
 			@print_line = ""
 		end
 	end
 
-	def search_rating(film_title)
-		rating = Imdb::Search.new(film_title).movies.first.rating
-		push_film_stars(rating.to_i)
+	def print_titles
+		index = 1
+		@list_of_films.each do |title|
+			puts index.to_s + ". " + title
+			index += 1
+		end
 	end
+
 end
 
-Rating.new('FilmsList.txt').print_stars
+R =Rating.new('FilmsList.txt')
+R.print_stars
+R.print_titles
--- Movie.rb
+++ Movie.rb
@@ -1,1 +1,27 @@
 class Movie
+	attr_accessor :title
+	attr_accessor :rating
+	attr_accessor :index
+
+	def initialize (title)
+		@title = title
+		@rating = get_rating
+	end
+
+	def push_film_stars
+		@film_stars = ""
+		@film_stars << "*" * self.rating + " " * (10 - self.rating)
+	end
+
+	def title
+		@title
+	end
+
+	def index
+		@index
+	end
+
+	def get_rating
+		rating = Imdb::Search.new(@title).movies.first.rating
+	end
+end
